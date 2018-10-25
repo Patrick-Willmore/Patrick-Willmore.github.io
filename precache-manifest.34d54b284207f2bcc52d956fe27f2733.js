f5034d415b1b9c4930c67581c21f0f593d4e1758
Lenovo <patman1127@gmail.com>
2018-10-25 14:11:32 -0600

    r draft

---
+++ precache-manifest.34d54b284207f2bcc52d956fe27f2733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "864452c9792a9ba49110",
+    "url": "/static/js/main.864452c9.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "864452c9792a9ba49110",
+    "url": "/static/css/main.1be9cc7e.chunk.css"
+  },
+  {
+    "revision": "7361aa57da2d7e8c148d26203e514611",
+    "url": "/index.html"
+  }
+];
